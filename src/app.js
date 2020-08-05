0ec9d52573bce9c81f8a3002c4e0f88932072a5a
icv852 <icv852@gmail.com>
2020-08-05 17:51:36 +0800

    Finish all functions

--- src/app.js
+++ src/app.js
@@ -1,62 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from my dashboard component
-    </div>
+const store = configureStore()
+
+store.subscribe(() => {
+    const state = store.getState()
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+    console.log(visibleExpenses)
+})
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))  
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }))
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))  
+
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
 )
 
-const AddExpensePage = () => (
-    <div>
-        This is from my add expense component
-    </div>
-)
-
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense component
-    </div>
-)
-
-const HelpPage = () => (
-    <div>
-        This is from my help component
-    </div>
-)
-
-const NotFoundPage = () => (
-    <div>
-        404! - <Link to="/">Go home</Link>
-    </div>
-)
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </header>
-)
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true} />
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>        
-    </BrowserRouter>
-)
-
-ReactDOM.render(routes, document.getElementById('app'))
+ReactDOM.render(jsx, document.getElementById('app'))
---
+++ src/playground/redux-expensify.js
@@ -0,0 +1,58 @@
+import { createStore, combineReducers } from 'redux'
+
+store.subscribe(() => {
+    const state = store.getState()
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+    console.log(visibleExpenses)
+})
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }))
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }))
+
+
+// console.log(expenseOne.expense.id)
+
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
+
+// store.dispatch(setTextFilter('rent'))
+// store.dispatch(setTextFilter())
+
+// store.dispatch(sortByAmount())
+// store.dispatch(sortByDate())
+
+// store.dispatch(setStartDate(125))
+// store.dispatch(setStartDate())
+// store.dispatch(setEndDate(1250))
+
+
+
+
+
+
+// const demoState = {
+//     expenses: [{
+//         id: 'poijljoijwer',
+//         description: 'January Rent',
+//         note: 'This was the final payment for that address',
+//         amount: 54500,
+//         createdAt: 0
+//     }],
+//     filters: {
+//         text: 'rent',
+//         sortBy: 'amount', // date or amount
+//         startDate: undefined,
+//         endDate: undefined
+//     }
+// }
+
+// const user = {
+//     name: 'Jen',
+//     age: 24
+// }
+
+// console.log({
+//     ...user,
+//     location: 'Philadelphia',
+//     age: 27
+// })
