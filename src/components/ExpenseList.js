65544e02178a1863a57937f2aa2da48dbd928358
icv852 <icv852@gmail.com>
2020-08-06 01:02:00 +0800

    Show total expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,14 +3,24 @@ import { connect } from 'react-redux'
 import ExpenseListItem from './ExpenseListItem'
 import selectExpenses from '../selectors/expenses'
 
-const ExpenseList = (props) => (
-    <div>
+const ExpenseList = (props) => {
+    const amountsArray = props.expenses.map(expense => expense.amount)
+    const addAmounts = (a, b) => {
+        return a + b
+    }
+    
+
+    return (<div>
         <h1>Expense List</h1>
         {props.expenses.map((expense) => {
             return <ExpenseListItem key={expense.id} {...expense}/>
         })}
-    </div>
-)
+        {/* Show total expense */}
+        <p>
+            Your total expense is: ${amountsArray.length !== 0 ? (amountsArray.reduce(addAmounts) / 100) : 0}            
+        </p>
+    </div>)
+}
 
 const mapStateToProps = (state) => {
     return {
